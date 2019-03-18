6f244467199a2023362b5cc471ac219194acc508
Alexandre Segura <mex.zktk@gmail.com>
2019-03-18 21:09:27 +0100

    Create access token before API calls.

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -5,12 +5,14 @@ require_once __DIR__ . '/HttpClientException.php';
 class CoopCycle_HttpClient
 {
     private $base_url;
-    private $api_token;
+    private $api_key;
+    private $api_secret;
 
     public function __construct()
     {
         $this->base_url = get_option('coopcycle_base_url');
-        $this->api_token = get_option('coopcycle_api_token');
+        $this->api_key = get_option('coopcycle_api_key');
+        $this->api_secret = get_option('coopcycle_api_secret');
     }
 
     public function is_successful($response_code)
@@ -22,9 +24,11 @@ class CoopCycle_HttpClient
     {
         $url = $this->base_url . $uri;
 
+        $access_token = $this->accessToken();
+
         $headers = array(
             'Content-Type' => 'application/ld+json',
-            'Authorization' => sprintf('Bearer %s', $this->api_token)
+            'Authorization' => sprintf('Bearer %s', $access_token)
         );
 
         $response = wp_remote_get($url, array(
@@ -47,9 +51,11 @@ class CoopCycle_HttpClient
     {
         $url = $this->base_url . $uri;
 
+        $access_token = $this->accessToken();
+
         $headers = array(
             'Content-Type' => 'application/ld+json',
-            'Authorization' => sprintf('Bearer %s', $this->api_token)
+            'Authorization' => sprintf('Bearer %s', $access_token)
         );
 
         $response = wp_remote_post($url, array(
@@ -67,5 +73,32 @@ class CoopCycle_HttpClient
         $body = wp_remote_retrieve_body($response);
 
         return json_decode($body, true);
+    }
+
+    public function accessToken()
+    {
+        $url = $this->base_url . '/oauth2/token';
+
+        $headers = array(
+            'Authorization' => sprintf('Basic %s', base64_encode(sprintf('%s:%s', $this->api_key, $this->api_secret)))
+        );
+
+        $response = wp_remote_post($url, array(
+            'timeout' => 30,
+            'sslverify' => false,
+            'headers' => $headers,
+            'body' => 'grant_type=client_credentials&scope=deliveries',
+        ));
+
+        $response_code = wp_remote_retrieve_response_code($response);
+        if (is_wp_error($response) || !$this->is_successful($response_code)) {
+            throw new HttpClientException($response);
+        }
+
+        $body = wp_remote_retrieve_body($response);
+
+        $data = json_decode($body, true);
+
+        return $data['access_token'];
     }
 }
