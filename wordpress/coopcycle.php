c548c4ff549f6091657dc314c4edbd4fd7a6e7d3
Alexandre Segura <mex.zktk@gmail.com>
2019-12-18 16:55:47 +0100

    Use time slots from store.
    - Show a dropdown to choose a time slot.
    - Remove JavaScript code to choose shipping time.

--- wordpress/coopcycle.php
+++ wordpress/coopcycle.php
@@ -17,70 +17,42 @@ if (is_admin()) {
     add_filter('plugin_action_links_' . plugin_basename( __FILE__ ), array($settings_page, 'add_action_link'));
 }
 
-function coopcycle_shipping_dates(\DateTime $now = null) {
+function coopcycle_shipping_date_options() {
 
-    if (null === $now) {
-        $now = new \DateTime();
-    }
+    $http_client = new CoopCycle_HttpClient();
 
-    $openingHoursSpecification =
-        json_decode(file_get_contents(__DIR__ . '/opening-hours.json'), true);
+    $options = array();
 
-    $daysOfWeek = array(
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday',
-        'Sunday',
-    );
+    try {
 
-    foreach ($openingHoursSpecification as $key => $openingHours) {
-        foreach ($daysOfWeek as $dayOfWeek) {
-            if (1 === preg_match("/${dayOfWeek}/", $openingHours['dayOfWeek'])) {
-                $openingHoursSpecification[$key]['dayOfWeek'] = $dayOfWeek;
+        $me = $http_client->get('/api/me');
+        $store = $http_client->get($me['store']);
+
+        if (isset($store['timeSlot'])) {
+
+            $time_slot = $http_client->get($store['timeSlot']);
+
+            $date_periods = CoopCycle::time_slot_to_date_periods($time_slot);
+            foreach ($date_periods as $date_period) {
+                $value = sprintf('%s %s-%s',
+                    $date_period->getStartDate()->format('Y-m-d'),
+                    $date_period->getStartDate()->format('H:i'),
+                    $date_period->getEndDate()->format('H:i')
+                );
+                $label = sprintf(__('%s between %s and %s'),
+                    date_i18n('l d F', $date_period->getStartDate()->getTimestamp()),
+                    $date_period->getStartDate()->format('H:i'),
+                    $date_period->getEndDate()->format('H:i')
+                );
+                $options[$value] = $label;
             }
         }
+
+    } catch (\Exception $e) {
+        // var_dump($e->getMessage());
     }
 
-    $candidates = array();
-    $storage = new \SplObjectStorage();
-    foreach ($openingHoursSpecification as $key => $openingHours) {
-
-        $date = clone $now;
-        $expression = sprintf('next %s', strtolower($openingHours['dayOfWeek']));
-        $date->modify($expression);
-
-        $storage[$date] = $openingHours;
-        $candidates[] = $date;
-    }
-
-    sort($candidates);
-
-    $shipping_dates = array();
-    foreach ($candidates as $shipping_date) {
-
-        $opening_hours = $storage[$shipping_date];
-
-        $pattern = '/([0-9]+):([0-9]+):([0-9]+)/';
-
-        $opens = clone $shipping_date;
-        $closes = clone $shipping_date;
-
-        preg_match($pattern, $opening_hours['opens'], $matches);
-        $opens->setTime($matches[1], $matches[2], $matches[3]);
-
-        preg_match($pattern, $opening_hours['closes'], $matches);
-        $closes->setTime($matches[1], $matches[2], $matches[3]);
-
-        $shipping_dates[] = array(
-            'opens' => $opens,
-            'closes' => $closes,
-        );
-    }
-
-    return $shipping_dates;
+    return $options;
 }
 
 function coopcycle_load_plugin_textdomain() {
@@ -114,7 +86,7 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
             return;
         }
 
-        if (!$_POST['shipping_date'] || !$_POST['shipping_time']) {
+        if (!$_POST['shipping_date']) {
             wc_add_notice(__('Please choose a shipping date.', 'coopcycle'), 'error');
         }
     }
@@ -128,9 +100,6 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
 
         if (!empty($_POST['shipping_date'])) {
             update_post_meta($order_id, 'shipping_date', sanitize_text_field($_POST['shipping_date']));
-        }
-        if (!empty($_POST['shipping_time'])) {
-            update_post_meta($order_id, 'shipping_time', sanitize_text_field($_POST['shipping_time']));
         }
     }
 
@@ -158,22 +127,6 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
      * @see https://docs.woocommerce.com/document/tutorial-customising-checkout-fields-using-actions-and-filters/
      */
     function coopcycle_checkout_fields($fields) {
-
-        $shipping_dates = coopcycle_shipping_dates();
-
-        $shipping_date_options = array();
-        $shipping_times_by_shipping_date = array();
-        foreach ($shipping_dates as $shipping_date) {
-            $opens = $shipping_date['opens'];
-            $closes = $shipping_date['closes'];
-
-            $shipping_date_options[$opens->format('Y-m-d')] = date_i18n('l d F', $opens->getTimestamp());
-            $shipping_times_by_shipping_date[$opens->format('Y-m-d')] = array(
-                'opens' => $opens->format('Y-m-d H:i:s'),
-                'closes' => $closes->format('Y-m-d H:i:s'),
-            );
-        }
-
         $fields['billing']['shipping_date'] = array(
             'type'      => 'select',
             'label'     => __('Shipping date', 'coopcycle'),
@@ -183,22 +136,7 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
             'class'     => array('form-row-wide', 'form-row-hidden'),
             'clear'     => true,
             'priority'  => 120,
-            'options'   => $shipping_date_options,
-            'custom_attributes' => array(
-                'data-shipping-time' => json_encode($shipping_times_by_shipping_date)
-            )
-        );
-
-        $fields['billing']['shipping_time'] = array(
-            'type'         => 'text',
-            'label'        => __('Shipping time', 'coopcycle'),
-            'required'     => true,
-            // Field is hidden by default, it will be shown via JavaScript
-            // @see coopcycle_after_shipping_rate
-            'class'        => array('form-row-wide', 'form-row-hidden'),
-            'clear'        => true,
-            'priority'     => 130,
-            'autocomplete' => false
+            'options'   => coopcycle_shipping_date_options(),
         );
 
         return $fields;
@@ -215,7 +153,6 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
             echo '<script>';
             echo 'document.querySelector("#shipping_date_time_heading").classList.remove("form-row-hidden");';
             echo 'document.querySelector("#shipping_date_field").classList.remove("form-row-hidden");';
-            echo 'document.querySelector("#shipping_time_field").classList.remove("form-row-hidden");';
             echo '</script>';
         }
 
@@ -235,48 +172,28 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
     function coopcycle_manage_shop_order_posts_columns($columns) {
         return array_merge($columns, array(
             'order_shipping_date' => __('Shipping date', 'coopcycle'),
-            'order_shipping_time' => __('Shipping time', 'coopcycle')
         ));
     }
     function coopcycle_manage_shop_order_posts_custom_column($column, $post_id) {
 
         $shipping_date = get_post_meta($post_id, 'shipping_date', true);
-        $shipping_time = get_post_meta($post_id, 'shipping_time', true);
 
-        $shipping_timestamp = false;
-        if ($shipping_date && $shipping_time) {
-            $shipping_timestamp = strtotime("{$shipping_date} {$shipping_time}");
-        }
+        // FIXME Make this backwards compatible
 
-        switch ($column) {
-            case 'order_shipping_date':
-                if ($shipping_timestamp) {
-                    echo date_i18n(get_option('date_format'), $shipping_timestamp);
-                }
-                break;
-            case 'order_shipping_time':
-                if ($shipping_timestamp) {
-                    echo date_i18n('H:i', $shipping_timestamp);
-                }
-                break;
+        if ($column === 'order_shipping_date') {
+
+            if ($shipping_date = get_post_meta($post_id, 'shipping_date', true)) {
+                // TODO Format as human readable
+                echo $shipping_date;
+            }
         }
     }
     add_filter('manage_shop_order_posts_columns', 'coopcycle_manage_shop_order_posts_columns', 20);
     add_action('manage_shop_order_posts_custom_column', 'coopcycle_manage_shop_order_posts_custom_column', 10, 2);
 
     function coopcycle_enqueue_scripts() {
-
-        wp_register_style('rome', 'https://cdnjs.cloudflare.com/ajax/libs/rome/2.1.22/rome.min.css');
-        wp_register_script('rome', 'https://cdnjs.cloudflare.com/ajax/libs/rome/2.1.22/rome.min.js');
-
         wp_register_style('coopcycle', plugins_url('/css/coopcycle.css', __FILE__), array(), false);
-        wp_register_script('coopcycle', plugins_url('/js/coopcycle.js', __FILE__), array(), false, true);
-
-        wp_enqueue_style('rome');
-        wp_enqueue_script('rome');
-
         wp_enqueue_style('coopcycle');
-        wp_enqueue_script('coopcycle');
     }
 
     add_action('wp_enqueue_scripts', 'coopcycle_enqueue_scripts');
@@ -292,7 +209,6 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
             }
 
             $shipping_date = $order->get_meta('shipping_date', true);
-            $shipping_time = $order->get_meta('shipping_time', true);
 
             // Array
             // (
@@ -317,7 +233,7 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
                         $shipping_address['postcode'],
                         $shipping_address['city']
                     ),
-                    'doneBefore' => sprintf('%s %s', $shipping_date, $shipping_time),
+                    'timeSlot' => $shipping_date,
                 )
             );
 
