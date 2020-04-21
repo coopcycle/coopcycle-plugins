e016bda58086434f75fd77ef957f169ba388966e
Alexandre Segura <mex.zktk@gmail.com>
2020-04-21 17:42:27 +0200

    Stop using API to calculate shipping rate. Fixes #11.
    - Extend WC_Shipping_Flat_Rate.

--- wordpress/src/ShippingMethod.php
+++ wordpress/src/ShippingMethod.php
@@ -3,38 +3,36 @@
 /**
  * @see https://github.com/woocommerce/woocommerce/blob/master/includes/abstracts/abstract-wc-shipping-method.php
  * @see https://github.com/woocommerce/woocommerce/blob/master/includes/shipping/flat-rate/class-wc-shipping-flat-rate.php
+ * @see https://github.com/woocommerce/woocommerce/blob/master/includes/shipping/flat-rate/includes/settings-flat-rate.php
  */
 if (!class_exists('CoopCycle_ShippingMethod')) {
 
-    class CoopCycle_ShippingMethod extends WC_Shipping_Method {
+    class CoopCycle_ShippingMethod extends WC_Shipping_Flat_Rate {
 
         private $http_client;
 
         public function __construct($instance_id = 0)
         {
-            parent::__construct($instance_id);
+            // Do *NOT* call parent constructor!
+            // It would call woocommerce_update_options_shipping_* with flat_rate
 
-            $this->id = 'coopcycle_shipping_method';
-            $this->title = __('CoopCycle', 'coopcycle');
-            $this->method_title = __('CoopCycle', 'coopcycle');
+            $this->id                 = 'coopcycle_shipping_method';
+            $this->instance_id        = absint($instance_id);
+            $this->title              = __('CoopCycle', 'coopcycle');
+            $this->method_title       = __('CoopCycle', 'coopcycle');
             $this->method_description = __('Allow customers to get delivered by a local coop running CoopCycle', 'coopcycle');
-
-            // - shipping-zones Shipping zone functionality + instances
-            // - instance-settings Instance settings screens.
-            // - settings Non-instance settings screens. Enabled by default for BW compatibility with methods before instances existed.
-            // - instance-settings-modal Allows the instance settings to be loaded within a modal in the zones UI.
-            $this->supports = array_merge(array(
+            $this->supports           = array(
                 'shipping-zones',
                 'instance-settings',
                 'instance-settings-modal',
-            ), $this->supports);
+            );
 
             // TODO The plugin should be enabled only if it has been configured
             $this->enabled = "yes";
 
             $this->init();
 
-            $this->http_client = CoopCycle::http_client();
+            add_action('woocommerce_update_options_shipping_' . $this->id, array($this, 'process_admin_options'));
         }
 
         public static function instance() {
@@ -50,38 +48,33 @@ if (!class_exists('CoopCycle_ShippingMethod')) {
             return false;
         }
 
-        public function init_form_fields()
+        public function get_instance_form_fields()
         {
-            $this->instance_form_fields = array(
-                'title' => array(
-                    'title' => __( 'Title', 'coopcycle' ),
-                    'type' => 'text',
-                    'description' => __( 'This controls the title which the user sees during checkout.', 'woocommerce' ),
-                    'default' => __('CoopCycle', 'coopcycle'),
-                    'desc_tip' => true,
-                ),
-            );
-        }
+            $instance_form_fields = parent::get_instance_form_fields();
 
-        protected function init()
-        {
-            $this->init_form_fields();
+            foreach ($instance_form_fields as $key => $value) {
+                if ($key === 'title') {
+                    $instance_form_fields[$key]['default'] = __('CoopCycle', 'coopcycle');
+                }
+            }
 
-            add_action('woocommerce_update_options_shipping_' . $this->id, array($this, 'process_admin_options'));
+            return $instance_form_fields;
         }
 
         public function get_shipping_date_options()
         {
+            $http_client = CoopCycle::http_client();
+
             $options = array();
 
             try {
 
-                $me = $this->http_client->get('/api/me');
-                $store = $this->http_client->get($me['store']);
+                $me = $http_client->get('/api/me');
+                $store = $http_client->get($me['store']);
 
                 if (isset($store['timeSlot'])) {
 
-                    $time_slot = $this->http_client->get($store['timeSlot']);
+                    $time_slot = $http_client->get($store['timeSlot']);
 
                     $date_periods = CoopCycle::time_slot_to_date_periods($time_slot);
                     foreach ($date_periods as $date_period) {
@@ -104,60 +97,6 @@ if (!class_exists('CoopCycle_ShippingMethod')) {
             }
 
             return $options;
-        }
-
-        public function calculate_shipping($package = array())
-        {
-            $options = $this->get_shipping_date_options();
-
-            // If there are no choices, skip this shipping method
-            if (empty($options)) {
-                return;
-            }
-
-            // country
-            // state
-            // postcode
-            // city
-            // address
-            // address_2
-            $destination = $package['destination'];
-
-            if (!is_array($destination)) {
-                return;
-            }
-
-            $params = [
-                'dropoffAddress' => trim(sprintf('%s %s %s',
-                    $destination['address'],
-                    $destination['postcode'],
-                    $destination['city']
-                )),
-            ];
-
-            $uri = sprintf('/api/pricing/calculate-price?%s', http_build_query($params));
-
-            try {
-
-                $cost = $this->http_client->get($uri);
-
-                if ($cost) {
-
-                    $rate = array(
-                        'id' => $this->get_rate_id(),
-                        'label' => $this->get_option('title'),
-                        'cost' => number_format($cost / 100, 2),
-                        'package' => $package,
-                        // 'taxes' => '???',
-                        'calc_tax' => 'per_order'
-                    );
-
-                    $this->add_rate($rate);
-                }
-
-            } catch (HttpClientException $e) {
-                // TODO Should we show a message to the user?
-            }
         }
     }
 }
